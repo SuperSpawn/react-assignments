2c5b0939a33f8c94e1b83f2ef8490251124aa699
SuperSpawn <thearktitan@gmail.com>
2023-02-14 15:45:13 +0200

    5.1

---
+++ assignment5.1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './components/Button.css'
+
+function App() {
+  return (
+    <div className="container">
+      <button className="btn important">Important</button>
+      <button className="btn">Not Important</button>
+    </div>
+  );
+}
+
+export default App;
