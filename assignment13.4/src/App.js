1562bd0f02a677a6eaf6fc68aa20e52904f3c27a
SuperSpawn <thearktitan@gmail.com>
2023-02-20 15:39:13 +0200

    13.4

---
+++ assignment13.4/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Spinner from './components/Spinner'
+
+function App() {
+  return (
+    <div className="container">
+        <Spinner/>
+    </div>
+  );
+}
+
+export default App;
