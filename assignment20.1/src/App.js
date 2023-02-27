e80cad3e19d610ec151987f32f2c6767856c44c0
SuperSpawn <thearktitan@gmail.com>
2023-02-27 20:53:21 +0200

    20.1

---
+++ assignment20.1/src/App.js
@@ -0,0 +1,49 @@
+import './styles/reset.css'
+import "./App.css";
+
+import { Component } from 'react';
+
+class Button extends Component {
+  
+  
+  render() {
+    return (
+      <button className="Button" onClick={this.props.clickHandler} className='Button'>{this.props.name}</button>
+    )
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { counter: 0 }
+  }
+  getCounterClass() {
+    if(this.state.counter > 0) return "App-counter-positive"
+    else if(this.state.counter < 0) return "App-counter-negative"
+    else return "App-counter-neutral"
+  }
+  incrementCounter() {
+    if(this.state.counter < 10) {
+      this.setState({counter: this.state.counter+1});
+    }
+  }
+  decrementCounter() {
+    if(this.state.counter > -10) {
+      this.setState({counter: this.state.counter-1});
+    }
+  }
+  render() { 
+    return (
+      <div className='App'>
+        <Button clickHandler={this.decrementCounter.bind(this)} name="-"/>
+        <p className={this.getCounterClass()}>{this.state.counter}</p>
+        <Button clickHandler={this.incrementCounter.bind(this)} name="+"/>
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
