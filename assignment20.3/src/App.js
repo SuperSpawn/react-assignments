26a254b526438bb48ab13256ca9488887799e997
SuperSpawn <thearktitan@gmail.com>
2023-02-27 22:53:18 +0200

    20.3

---
+++ assignment20.3/src/App.js
@@ -0,0 +1,39 @@
+import './styles/reset.css'
+import "./App.css";
+
+import { Component } from 'react';
+
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {seconds: 0, minutes: 0, hours: 0} 
+  }
+
+  handleSeconds(value) {
+    this.setState({seconds: value, minutes:value/60, hours:value/3600})
+  }
+  handleMinutes(value) {
+    this.setState({seconds: value*60, minutes:value, hours:value/60})
+  }
+  handleHours(value) {
+    this.setState({seconds: value*3600, minutes:value*60, hours:value})
+  }
+
+  render() {
+
+    const secondsHandler = this.handleSeconds.bind(this);
+    const minutesHandler = this.handleMinutes.bind(this);
+    const hoursHandler = this.handleHours.bind(this);
+
+    return (
+      <div className='App'>
+        <input onChange={(e) => secondsHandler(Number.parseInt(e.target.value))} type="text" value={this.state.seconds}/>
+        <input onChange={(e) => minutesHandler(Number.parseInt(e.target.value))} type="text" value={this.state.minutes}/>
+        <input onChange={(e) => hoursHandler(Number.parseInt(e.target.value))} type="text" value={this.state.hours}/>
+      </div>
+    )
+  }
+}
+
+export default App;
