7f001f045f52deae4ca226bd112a92c1ffb06d0c
SuperSpawn <thearktitan@gmail.com>
2023-02-28 13:57:15 +0200

    21.2

---
+++ assignment21.2/src/App.js
@@ -0,0 +1,29 @@
+import "./styles/reset.css";
+import "./App.css";
+
+import { Component } from "react";
+import CountryList from "./components/CountryList";
+
+class App extends Component {
+  state = { filterString: "" };
+
+  setFilterString(e) {
+    this.setState({ filterString: e.target.value });
+  }
+
+  render() {
+
+    return (
+      <div className="App-container">
+        <input
+          onChange={this.setFilterString.bind(this)}
+          type="search"
+          placeholder="filter"
+        />
+        <CountryList filterString={this.state.filterString} />
+      </div>
+    );
+  }
+}
+
+export default App;
